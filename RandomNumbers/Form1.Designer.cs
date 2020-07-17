1205bd8c2f937a5de0679cdb4c60ccf93a392de1
Shary <a.gurzhyi@gmail.com>
2020-07-18 00:35:05 +0300

    Completed

--- RandomNumbers/Form1.Designer.cs
+++ RandomNumbers/Form1.Designer.cs
@@ -28,13 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudFrom = new System.Windows.Forms.NumericUpDown();
+            this.nudTo = new System.Windows.Forms.NumericUpDown();
+            this.tbOutPut = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbHistory = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.cbWithoutRepeat = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.BackColor = System.Drawing.Color.Lavender;
+            this.btnRandom.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnRandom.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnRandom.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnRandom.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnRandom.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRandom.Location = new System.Drawing.Point(124, 115);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(97, 36);
+            this.btnRandom.TabIndex = 0;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = false;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To";
+            // 
+            // nudFrom
+            // 
+            this.nudFrom.Location = new System.Drawing.Point(86, 41);
+            this.nudFrom.Name = "nudFrom";
+            this.nudFrom.Size = new System.Drawing.Size(66, 20);
+            this.nudFrom.TabIndex = 3;
+            // 
+            // nudTo
+            // 
+            this.nudTo.Location = new System.Drawing.Point(86, 70);
+            this.nudTo.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudTo.Name = "nudTo";
+            this.nudTo.Size = new System.Drawing.Size(66, 20);
+            this.nudTo.TabIndex = 4;
+            this.nudTo.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // tbOutPut
+            // 
+            this.tbOutPut.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbOutPut.Location = new System.Drawing.Point(40, 117);
+            this.tbOutPut.Name = "tbOutPut";
+            this.tbOutPut.Size = new System.Drawing.Size(57, 32);
+            this.tbOutPut.TabIndex = 5;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Lavender;
+            this.btnReset.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnReset.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnReset.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnReset.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.btnReset.Location = new System.Drawing.Point(187, 26);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(67, 33);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(65, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "History:";
+            // 
+            // tbHistory
+            // 
+            this.tbHistory.AutoCompleteCustomSource.AddRange(new string[] {
+            "1",
+            "0"});
+            this.tbHistory.BackColor = System.Drawing.Color.GhostWhite;
+            this.tbHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbHistory.Location = new System.Drawing.Point(67, 205);
+            this.tbHistory.Multiline = true;
+            this.tbHistory.Name = "tbHistory";
+            this.tbHistory.ReadOnly = true;
+            this.tbHistory.Size = new System.Drawing.Size(57, 107);
+            this.tbHistory.TabIndex = 8;
+            this.tbHistory.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Lavender;
+            this.btnClear.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnClear.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnClear.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnClear.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(142, 267);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(85, 34);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.BackColor = System.Drawing.Color.Lavender;
+            this.btnCopy.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCopy.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnCopy.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnCopy.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnCopy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(204)));
+            this.btnCopy.Location = new System.Drawing.Point(142, 217);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(85, 34);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "Copy ";
+            this.btnCopy.UseVisualStyleBackColor = false;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // cbWithoutRepeat
+            // 
+            this.cbWithoutRepeat.AutoSize = true;
+            this.cbWithoutRepeat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbWithoutRepeat.Location = new System.Drawing.Point(147, 181);
+            this.cbWithoutRepeat.Name = "cbWithoutRepeat";
+            this.cbWithoutRepeat.Size = new System.Drawing.Size(127, 21);
+            this.cbWithoutRepeat.TabIndex = 9;
+            this.cbWithoutRepeat.Text = "Without repeats";
+            this.cbWithoutRepeat.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(300, 333);
+            this.Controls.Add(this.cbWithoutRepeat);
+            this.Controls.Add(this.tbHistory);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.tbOutPut);
+            this.Controls.Add(this.nudTo);
+            this.Controls.Add(this.nudFrom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRandom);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nudFrom;
+        private System.Windows.Forms.NumericUpDown nudTo;
+        private System.Windows.Forms.TextBox tbOutPut;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbHistory;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.CheckBox cbWithoutRepeat;
     }
 }
 
--- RandomNumbers/Form1.cs
+++ RandomNumbers/Form1.cs
@@ -12,9 +12,53 @@ namespace RandomNumbers
 {
     public partial class Form1 : Form
     {
+        Random random;
+       
         public Form1()
         {
+            random = new Random();
             InitializeComponent();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            int from = (int)nudFrom.Value;
+            int to = (int)nudTo.Value;
+            if (from < to)
+            {
+                int number = random.Next(from, to);
+                tbOutPut.BackColor = Color.AliceBlue;
+                tbOutPut.Text = number.ToString();
+                if (cbWithoutRepeat.Checked)
+                {
+                    if (tbHistory.Text.IndexOf(number.ToString()) == -1)
+                        tbHistory.AppendText(tbOutPut.Text + "\n");
+                }
+                else
+                    tbHistory.AppendText(tbOutPut.Text + "\n");
+            }
+            else
+            {
+                MessageBox.Show("Invalid arguments", "Error");
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            nudFrom.Value = 0;
+            nudTo.Value = 6;
+            tbOutPut.BackColor = Color.White;
+            tbOutPut.Text = null;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbHistory.Text = null;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(tbHistory.Text);
         }
     }
 }
